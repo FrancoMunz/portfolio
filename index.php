510184e4ea20668ecdc95009de3f8bf7448edd93
FrancoMunz <francomunz@hotmail.com>
2022-05-19 20:08:06 -0300

    cambios

---
+++ index.php
@@ -0,0 +1,89 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="css/bootstrap/css/bootstrap.min.css">
+    <link rel="stylesheet" href="css/fontawesome/css/all.min.css">
+    <link rel="stylesheet" href="css/fontawesome/css/fontawesome.min.css">
+    <link href="https://fonts.googleapis.com/css?family=Montserrat:100,200,300,400,500,600,700,800,900&display=swap"
+        rel="stylesheet">
+    <link rel="stylesheet" href="css/estilos.css">
+    <link rel="shortcut icon" href="images/favicon.ico"/>
+    <title>Inicio</title>
+</head>
+<body id="inicio">
+    <header>
+        <header class="container">
+            <nav class="navbar navbar-expand-md mb-4">
+                <div class="container-fluid">
+                    <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarCollapse"
+                        aria-controls="navbarCollapse" aria-expanded="false" aria-label="Toggle navigation">
+                        <i class="fa-solid fa-bars"></i>
+                    </button>
+                    <div class="collapse navbar-collapse" id="navbarCollapse">
+                        <ul class="navbar-nav me-auto mb-2 mb-md-0">
+                            <li class="nav-item">
+                                <a class="nav-link active" aria-current="page" href="index.html">Inicio</a>
+                            </li>
+                            <li class="nav-item">
+                                <a class="nav-link" href="sobre-mi.php">Sobre Mí</a>
+                            </li>
+                            <li class="nav-item">
+                                <a class="nav-link" href="contacto.php">Contacto</a>
+                            </li>
+                            <li class="nav-item">
+                                <a class="nav-link" href="proyectos.php">Proyectos</a>
+                            </li>
+                        </ul>
+                        <div class="d-flex">
+                            <button class="btn btn-rojo" type="submit"> <i class="fa-solid fa-file-zipper"></i> Descargar mi
+                                CV </button>
+                        </div>
+                    </div>
+                </div>
+            </nav>
+        </header>   
+<main class="container">
+        <div class="row">
+            <div class="col-12 mt-sm-5 text-center div-cohete">
+                <a href="proyectos.php"><img src="imagenes/cohete.svg" class="cohete"></a>
+            </div>
+        </div>
+        <div class="row">
+            <div class="col-12 col-sm-6 my-4 mt-sm-5 text-center offset-sm-3 mb-3">
+                <div class="input-home">
+                    <p class="p-1">Bienvenid@ a mi portfolio de proyectos.</p>
+                </div>
+            </div>
+        </div>
+        <div class="row">
+            <div class="col-12 text-center">
+                <a href="proyectos.php" class="btn shadow">Conoce mis proyectos</a>
+            </div>
+        </div>
+    </main>
+    <footer class="container">
+        <div class="row mt-5 pb-3">
+            <div class="col-12 col-sm-3 text-center text-sm-start">
+                <a href="https://github.com" target="_blank" title="Github"><i class="fab fa-github"></i></a>
+                <a href="https://www.linkedin.com/in/francomunz/" target="_blank" title="Linkedin"><i
+                        class="fab fa-linkedin-in"></i></a>
+            </div>
+            <div class="col-12 col-sm-3 text-center text-sm-start pt-2 p-sm-0">
+                Sponsor <a href="https://depcsuite.com" target="_blank" title="DEPCSUITE">DePC Suite</a>
+            </div>
+            <div class="col-12 col-sm-3 text-sm-start  pt-2 p-sm-0">
+                <a href="mailto:francomunz@hotmail.com">francomunz@hotmail.com</a>
+            </div>
+        </div>
+    </footer>
+    <div class="whatsapp">
+        <a href="https://api.whatsapp.com/send?phone=542236193362" title="Whatsapp"><i class="fab fa-whatsapp"></i></a>
+    </div>
+    <script src="css/bootstrap/js/bootstrap.bundle.min.js"></script></body>
+<body>
+    
+</body>
+</html>
